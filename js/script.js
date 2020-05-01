fcdfa53145affbb5d59e9b6307ddffa5119b449d
Krzysztof Fila <kfila17@gmail.com>
2020-05-01 13:15:03 +0200

    fixed links, move loop for links

--- js/script.js
+++ js/script.js
@@ -26,12 +26,6 @@ function titleClickHandler(event) {
   targetArticle.classList.add("active");
 }
 
-const links = document.querySelectorAll(".titles a");
-
-for (let link of links) {
-  link.addEventListener("click", titleClickHandler);
-}
-
 const optArticleSelector = ".post",
   optTitleSelector = ".post-title",
   optTitleListSelector = ".titles";
@@ -39,28 +33,34 @@ const optArticleSelector = ".post",
 function generateTitleLinks() {
   /* [DONE] remove contents of titleList */
   const titleList = document.querySelector(optTitleListSelector);
-  titleList.innerHTML = "";
+
   /* [DONE] for each article */
   const articles = document.querySelectorAll(optArticleSelector);
-  /*[LOOP!?]*/
+  /*[DONE Loop]*/
   let html = "";
   for (let article of articles) {
     /*[DONE] get the article id */
-    const ArticleId = article.getAttribute("id");
+    const articleId = article.getAttribute("id");
     /*[DONE] find the title element */
     const articleTitle = article.querySelector(optTitleSelector).innerHTML;
-    /* [I CAN`T!]get the title from the title element */
+    /* [TO DO!]get the title from the title element */
 
-    /* create HTML of the link 
+    /* create HTML of the link */
     const linkHTML =
       '<li><a href="#' +
       articleId +
       '"><span>' +
       articleTitle +
-      "</span></a></li>";*/
-    /* insert link into titleList 
-    html = html + linkHTML;*/
+      "</span></a></li>";
+    /* insert link into titleList */
+    html = html + linkHTML;
   }
-  /*titleList.innerHTML = html;*/
+  titleList.innerHTML = html;
+  const links = document.querySelectorAll(".titles a");
+  console.log(links);
+
+  for (let link of links) {
+    link.addEventListener("click", titleClickHandler);
+  }
 }
 generateTitleLinks();
